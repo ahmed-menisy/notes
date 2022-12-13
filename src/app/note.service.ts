735a8fd04b5e46cafc4792ac31ca415ad136922c
ahmed-menisy <amelmaster16@gmail.com>
2022-12-13 15:34:33 +0200

    update api

--- src/app/note.service.ts
+++ src/app/note.service.ts
@@ -7,21 +7,21 @@ import { HttpClient } from '@angular/common/http';
 })
 export class NoteService {
   constructor(private http: HttpClient) {}
-  baseUrl: string = 'https://routeegypt.herokuapp.com/';
+  baseUrl: string = 'https://sticky-note-fe.vercel.app/';
   // -------- Add Note
   addNote(data: object): Observable<any> {
     return this.http.post(`${this.baseUrl}addNote`, data);
   }
   // -------- Get Note
-  getNoteUser(data:object):Observable<any> {
-    return this.http.post(`${this.baseUrl}getUserNotes`,data)
+  getNoteUser(data: object): Observable<any> {
+    return this.http.post(`${this.baseUrl}getUserNotes`, data);
   }
-  // --------- delet Note 
-  deleteNote(data:Object):Observable<any> {
-    return this.http.delete(`${this.baseUrl}deleteNote`,data)
+  // --------- delet Note
+  deleteNote(data: Object): Observable<any> {
+    return this.http.delete(`${this.baseUrl}deleteNote`, data);
   }
   // -------- update Note
-  updateNote(data:Object):Observable<any> {
-    return this.http.put(`${this.baseUrl}updateNote`,data)
+  updateNote(data: Object): Observable<any> {
+    return this.http.put(`${this.baseUrl}updateNote`, data);
   }
 }
